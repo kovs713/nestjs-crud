8f3b10a994c6651a587c1865c1a968648a8c1471
kovs713 <ovsyannikovkostyan@gmail.com>
2026-09-15 14:57:11 +0400

    refactor: update dto fields

--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -11,17 +11,17 @@ export class AuthLoginDto {
   @ApiPropertyOptional({ example: 'john@example.com', format: 'email' })
   @IsOptional()
   @IsEmail()
-  email: string;
+  readonly email: string;
 
   @ApiPropertyOptional({ example: 'john_doe', minLength: 6, maxLength: 30 })
   @IsOptional()
   @IsString()
   @Length(6, 30)
-  login: string;
+  readonly login: string;
 
   @ApiProperty({ example: 's3cr3t-p4ssword', minLength: 8, maxLength: 30 })
   @IsNotEmpty()
   @IsString()
   @Length(8, 30)
-  password: string;
+  readonly password: string;
 }
