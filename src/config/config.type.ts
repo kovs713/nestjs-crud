afbaa740ca48a8354d555b3def51111156ac1bb1
kovs713 <ovsyannikovkostyan@gmail.com>
2026-09-15 15:06:31 +0400

    feat: add s3 config

--- src/config/config.type.ts
+++ src/config/config.type.ts
@@ -1,6 +1,7 @@
 import { AuthConfig } from '@/auth/auth.config';
 import { CacheConfig } from '@/providers/cache/cache.config';
 import { DatabaseConfig } from '@/providers/database/database.config';
+import { S3Config } from '@/providers/files/s3/s3.config';
 import { AppConfig } from './app.config';
 
 export type AllConfigType = {
@@ -8,4 +9,5 @@ export type AllConfigType = {
   db: DatabaseConfig;
   auth: AuthConfig;
   redis: CacheConfig;
+  s3: S3Config;
 };
