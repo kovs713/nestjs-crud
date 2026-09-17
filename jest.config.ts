4c1716d711c62ccae798b8d02c54d6002de6fab8
kovs713 <ovsyannikovkostyan@gmail.com>
2026-09-17 18:51:12 +0400

    fix: double mocks issue

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 export default {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '.',
+  roots: ['<rootDir>/src/', '<rootDir>/test/'],
   testRegex: '.*\\.spec\\.ts$',
   transform: { '^.+\\.(t|j)s$': 'ts-jest' },
   testEnvironment: 'node',
