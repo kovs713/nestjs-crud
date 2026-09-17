bd8be31afea8f60efc23e963e61b01c7f9176669
kovs713 <ovsyannikovkostyan@gmail.com>
2026-09-17 19:22:23 +0400

    refactor: rename repository variable

--- src/features/users/users.service.ts
+++ src/features/users/users.service.ts
@@ -18,17 +18,17 @@ import { InsertUser, RawUser, UpdateUser } from './types/users.types';
 @Injectable()
 export class UsersService {
   constructor(
-    private readonly repository: UsersRepository,
+    private readonly users: UsersRepository,
     private readonly avatars: AvatarRepository,
     private readonly s3Service: IFileService,
   ) {}
 
   async search(dto: SearchUsersDto): Promise<RawUser[]> {
-    return this.repository.search(dto);
+    return this.users.search(dto);
   }
 
   async getById(id: string): Promise<RawUser> {
-    const user = await this.repository.findById(id);
+    const user = await this.users.findById(id);
 
     if (!user) throw new NotFoundException(`user ${id} not found`);
 
@@ -36,7 +36,7 @@ export class UsersService {
   }
 
   async getByLogin(login: string): Promise<RawUser> {
-    const user = await this.repository.findByLogin(login);
+    const user = await this.users.findByLogin(login);
 
     if (!user) throw new NotFoundException('user not found');
 
@@ -44,7 +44,7 @@ export class UsersService {
   }
 
   async getByEmail(email: string): Promise<RawUser> {
-    const user = await this.repository.findByEmail(email);
+    const user = await this.users.findByEmail(email);
 
     if (!user) throw new NotFoundException('user not found');
 
@@ -61,7 +61,7 @@ export class UsersService {
       description: dto.description,
     };
 
-    return this.repository.create(userData);
+    return this.users.create(userData);
   }
 
   async update(id: string, dto: UpdateUserDto): Promise<RawUser> {
@@ -74,14 +74,14 @@ export class UsersService {
       }),
     };
 
-    const user = await this.repository.updateById(id, userData);
+    const user = await this.users.updateById(id, userData);
     if (!user) throw new NotFoundException(`user ${id} not found`);
 
     return user;
   }
 
   async delete(id: string): Promise<RawUser> {
-    const user = await this.repository.softDeleteById(id);
+    const user = await this.users.softDeleteById(id);
 
     if (!user) throw new NotFoundException(`user ${id} not found`);
 
