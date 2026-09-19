48d23def6dc7e8839af0060456953346f8c0876d
kovs713 <ovsyannikovkostyan@gmail.com>
2026-09-19 19:01:51 +0400

    fix: providers

--- src/features/balance/balance.module.ts
+++ src/features/balance/balance.module.ts
@@ -3,13 +3,20 @@ import { Module } from '@nestjs/common';
 
 import { BALANCE_QUQUE } from './balance.constants';
 import { BalanceController } from './balance.controller';
+import { BalanceProcessor } from './balance.processor';
 import { BalanceRepository } from './repositories/balance.repository';
+import { BalanceCronService, BalanceQueueService } from './services';
 import { BalanceService } from './services/balance.service';
 
 @Module({
   imports: [BullModule.registerQueue({ name: BALANCE_QUQUE })],
   controllers: [BalanceController],
-  providers: [BalanceService, BalanceRepository],
-  exports: [BalanceService],
+  providers: [
+    BalanceService,
+    BalanceQueueService,
+    BalanceCronService,
+    BalanceProcessor,
+    BalanceRepository,
+  ],
 })
 export class BalanceModule {}
